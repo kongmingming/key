c97060bf3882eee647dc8d2d7e6573d8cb6d0ed7
kongmingming <1300934326@qq.com>
2019-09-04 08:54:36 +0800

    'day2技能'

---
+++ index.js
@@ -0,0 +1,13 @@
+let Arr = ['a', 2, 4, 5, 'h'];
+Arr.forEach((item, index) => {
+        // item.splice(1, index);
+        // item.filter(item)
+        console.log(item)
+    })
+    // Arr1.push(item)
+
+let Arr1 = ['aa', 2, 5, 6, 4];
+Arr1.forEach((item, index) => {
+    item[index].filter(0, index)
+    console.log(item)
+})
